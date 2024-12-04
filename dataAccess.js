4631fcc8963687afce9cd61b3f14abb3f0dccda6
Cerine :D <139589082+ceriiine@users.noreply.github.com>
2024-12-04 10:25:34 +0000

    initial project structure

---
+++ dataAccess.js
@@ -0,0 +1,41 @@
+function readTasks(fileName){
+
+}
+
+function writeTasks(fileName){
+
+}
+
+function getNextId(tasks){
+
+}
+
+//Add an element to the database
+function addTask(description){
+
+}
+
+//Delete and element from the database
+function deleteTask(id){
+
+}
+
+//Update an element in the database
+function updateTask(id, description){
+
+}
+
+//Mark done
+function markTaskInProgress(id){
+
+}
+
+//Mark in progress
+function markTaskDone(id){
+
+}
+
+//List elements with status
+function listTasks(status){
+
+}
